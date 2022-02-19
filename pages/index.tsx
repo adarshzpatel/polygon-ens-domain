08ab06d0beab943b961ee885c356f4440d2161c6
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-02-19 12:52:10 +0530

    init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-slate-900 text-white select-none flex flex-col`
+}
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+   
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      display: 'Space Grotesk',
+      body: 'IBM Plex Sans'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
